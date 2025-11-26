e1e8aa992bed3ea0f5b17dc093f46a23750848b9
Dinyar <migranovdiniar@gmail.com>
2025-11-26 03:22:23 +0300

    2

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 class WordPlay:
     def __init__(self, words: list):
         self.words = words
+
+    def add_word(self, word: str):
+        if word not in self.words:
+            self.words += [word]
+
+    def words_with_length(self, n: int):
+        return [i for i in self.words if len(i) == n]
+
+    def only(self, *args):
+        include = []
+        for i in args:
+            for j in self.words:
+                if i in j:
+                    include += [j]
+
+
+
